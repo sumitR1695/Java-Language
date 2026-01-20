e3be437f8b87ebd8eeeff86064fb01405f6b1c25
sumitR1695 <rautsumit7057@gmail.com>
2026-01-21 00:20:28 +0530

    Initial commit

---
+++ SwitchCase/TraffSignal.java
@@ -0,0 +1,20 @@
+class Signals{
+
+	public static void main(String[] args){
+	
+		String s1 = "Red";
+		String s2 = "Blue";
+
+		switch (s1){
+			case "Red":
+				System.out.println(s1+"- STOP");
+			break;
+		}
+		switch (s2){
+			case "Blue":
+				System.out.println(s2+"- No such colour is present in Traffic Lights");
+	
+		}
+
+	}
+}
